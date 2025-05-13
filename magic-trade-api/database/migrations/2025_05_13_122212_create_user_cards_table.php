9be7de10501b4757c558faa24dfdef673eca057a
MARKWITZ CLEMENT <CLEMENT.MARKWITZ@carsat-nordpicardie.fr>
2025-05-13 15:16:51 +0200

    ajout de la migration des cards des users avec l api

---
+++ magic-trade-api/database/migrations/2025_05_13_122212_create_user_cards_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('card_id', 40);
+            $table->string('finish', 20);
+            $table->integer('quantity')->default(1);
+            $table->boolean('trade')->default(false);
+            $table->boolean('etat');
+            $table->date('acquired_date')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+
+            $table->unique(['user_id', 'card_id', 'finish'], 'user_card_finish');
+            $table->foreign('card_id')->references('id')->on('cards')->onDelete('cascade');
+            $table->index(['user_id', 'for_trade','finish']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_cards');
+    }
+};
