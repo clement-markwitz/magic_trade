45593b666cc04a6d19df01a6b738de67543cb6b8
MARKWITZ CLEMENT <CLEMENT.MARKWITZ@carsat-nordpicardie.fr>
2025-05-14 12:13:43 +0200

    reglement bug mig client

--- magic-trade-api/database/migrations/2025_05_13_140939_create_clients_table.php
+++ magic-trade-api/database/migrations/2025_05_13_140939_create_clients_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->foreign('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name',20);
             $table->string('last_name',20);
             $table->string('pseudo',20);
