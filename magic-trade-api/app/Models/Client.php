099b644a4654c574887234e8decbc7bfd012e890
MARKWITZ CLEMENT <CLEMENT.MARKWITZ@carsat-nordpicardie.fr>
2025-05-14 12:01:11 +0200

    ajout model client

--- magic-trade-api/app/Models/Client.php
+++ magic-trade-api/app/Models/Client.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $table = "clients";
+    protected $fillable = [ 
+        'user_id','name','last_name','pseudo','contry','city','street','postal_code','phone','description'
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
