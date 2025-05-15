14904ffa864141ea4aed1112cfbe2b83e649bb17
MARKWITZ CLEMENT <CLEMENT.MARKWITZ@carsat-nordpicardie.fr>
2025-05-15 11:18:30 +0200

    création migration pour tradeitem

---
+++ magic-trade-api/database/migrations/2025_05_15_085749_create_trade_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trade_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trade_id')->constrained('trades')->cascadeOnDelete();
+            $table->foreignId('user_card_id')->constrained('user_cards')->cascadeOnDelete();
+            $table->foreignId('to_user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trade_items');
+    }
+};
