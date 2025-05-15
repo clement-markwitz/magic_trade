38463522392cb5649ac3827922d4627eea4b6846
MARKWITZ CLEMENT <CLEMENT.MARKWITZ@carsat-nordpicardie.fr>
2025-05-15 11:18:39 +0200

    création model pour tradeitem

---
+++ magic-trade-api/app/Models/TradeItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TradeItem extends Model
+{
+    protected $fillable = [
+        'trade_id','user_card_id','to_user_id'
+        ];
+    public function trade(){
+        return $this->belongsTo(Trade::class,'trade_id');
+    }
+    public function userCard(){
+        return $this->belongsTo(UserCard::class,'user_id');
+    }
+}
